b24a8296846761bd6da4008912df12bca4d136c4
rudyhuacasi <alexisitaliano16@gmail.com>
2024-07-30 17:10:03 +0200

    {UPD} added method to define the relationship many to many

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -19,4 +19,9 @@ class Project extends Model
     {
         return $this->belongsTo(Type::class);
     }
+    public function technologies()
+    {
+        return $this->belongsToMany(Technology::class);
+    }
+
 }
